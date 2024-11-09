6858d1e2b42ae42468ecdfc440f866694a6c5803
reza--boy <60574856+danelloptz@users.noreply.github.com>
2024-11-10 00:05:48 +0300

    Исправлены баги и добавлен вывод спарсенных данных по серийнику

--- frontend/main.js
+++ frontend/main.js
@@ -19,26 +19,28 @@ function isEmptyField(text) {
     return text.includes("Уточнить") || text == "None";
 }
 
-function sendMessage() {
+async function sendMessage() {
     const inputTheme = document.getElementById("messageInputTheme");
     const inputText = document.getElementById("messageInputText");
     const theme = inputTheme.value.trim();
     const text = inputText.value.trim();
+    let serial_number = "";
 
     if (text === "" || theme === "") return;
 
     addMessage(theme, text, "user");
     inputTheme.value = "";
-    inputText.value = "Введите текст сообщения...";
+    inputText.value = "";
+    inputText.ariaPlaceholder = "Введите текст сообщения...";
 
     const message = theme + " " + text;
 
-    fetch('/api/send', {
+    await fetch('/api/send', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ text: message })
+        body: JSON.stringify({ msg: message })
     })
     .then(response => {
         if (!response.ok) {
@@ -48,45 +50,68 @@ function sendMessage() {
         return response.json();
     })
     .then(data => { 
-        /*
-            я буду ожидать от вас json такой структуры:
-            {
-                failure_point: smth,
-                device_type: smth,
-                serial_number: smth
-            }
-            данные не заполены: ответ начинается со слов Укажите, либо в поле None
-        */
-       console.log(data);
-
         if (!isEmptyField(data.device_type) && !isEmptyField(data.failure_point) && !isEmptyField(data.serial_number)) {
-            let msg = `
-Здравствуйте,
+            const msg = `
+            Здравствуйте! <br>
 
-Ваше устройство: ${data.device_type}
-Точка неисправности: ${data.failure_point}
-Серийный номер: ${data.serial_number}
+            Ваше устройство: ${data.device_type}<br>
+            Точка неисправности: ${data.failure_point}<br>
+            Серийный номер: ${data.serial_number}<br>
 
-Ваша заявка принята в работу!
+            Ваша заявка принята в работу!<br>
 
-С уважением,
-Служба поддержки компании «Сила»
-`;
+            С уважением,
+            Служба поддержки компании «Сила»
+            `;
 
             addMessage("Все данные есть!", msg, "bot");
+            serial_number = data.serial_number; 
         } else {
-            let msg = "Здравствуйте! Пожалуйста, уточните следующие данные для продолжения обработки заявки:\n";
-            if (isEmptyField(data.device_type)) msg += "\tТип оборудования\n";
-            if (isEmptyField(data.failure_point)) msg += "\tТочка отказа\n";
-            if (isEmptyField(data.serial_number)) msg += "\tСерийный номер\n";
+            let msg = "Здравствуйте! Пожалуйста, уточните следующие данные для продолжения обработки заявки:<br>";
+            if (isEmptyField(data.device_type)) msg += "&nbsp;&nbsp;&nbsp;&nbsp; - Тип оборудования, <br>";
+            if (isEmptyField(data.failure_point)) msg += "&nbsp;&nbsp;&nbsp;&nbsp; - Точка отказа, <br>";
+            if (isEmptyField(data.serial_number)) msg += "&nbsp;&nbsp;&nbsp;&nbsp; - Серийный номер<br>";
 
             addMessage("Не хватает данных.", msg, "bot");
         }
     })
     .catch(error => {1
         console.error('Ошибка:', error);
-        addMessage("Произошла ошибка при получении ответа от сервера. Проверьте подключение или обратитесь к разработчику.", "", "bot");
+        addMessage("Произошла ошибка при получении ответа от сервера при запросе на данные метрик. Проверьте подключение или обратитесь к разработчику.", "", "bot");
     });
+
+    if (!isEmptyField(serial_number)) {
+        await fetch('/get_model_info', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ data: serial_number })
+        }).then(response => {
+            if (!response.ok) {
+                console.log(response);
+                throw new Error("Сервер вернул ошибку");
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (!isEmptyField(data.model_info)) {
+                const msg = 
+                `
+                    Вот, какую информацию я смог найти по вашему товару. Надеюсь, она будет полезна!
+                    ${data.model_info}
+                `;
+                addMessage("Данные найдены!", msg, "bot");
+            } else {
+                const msg = "К сожалению, я не смог найти информацию о вашем товаре в нашем каталоге.";
+                addMessage("Данные не найдены!", msg, "bot");
+            }
+        }).catch(error => {1
+            console.error('Ошибка:', error);
+            addMessage("Произошла ошибка при получении ответа от сервера при запросе по серийному номеру. Проверьте подключение или обратитесь к разработчику.", "", "bot");
+        });
+    }
+    
 }
 
 function addMessage(theme, text, sender) {
