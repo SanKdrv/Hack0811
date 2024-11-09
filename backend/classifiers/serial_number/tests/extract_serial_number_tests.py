f4de2beaeb6e08cf0cd26bd701eed8cdbc6955dc
Daniil Kusin <kasvorten98342@mail.ru>
2024-11-09 17:18:37 +0300

    tests serial clf added

--- backend/classifiers/serial_number/tests/extract_serial_number_tests.py
+++ backend/classifiers/serial_number/tests/extract_serial_number_tests.py
@@ -1,5 +1,6 @@
 import unittest
-from regular_extractor import RegularExtractor  # Импортируем класс
+from ..regular_extractor import RegularExtractor  # Импортируем класс
+
 
 class TestSerialNumberExtractor(unittest.TestCase):
     def setUp(self):
@@ -15,7 +16,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Прошу взять в ремонт ноутбук с серийным номером C223100360."
         expected = "C223100360"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_multiple_serial_numbers(self):
         """
@@ -23,7 +24,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Также прошу проверить серийные номера C223094534, D251110041 и C223014328."
         expected = "C223094534"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_russian_letters_and_mixed_case(self):
         """
@@ -31,7 +32,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Серийные номера с ошибками: с223091001, C223012430 и С111120102."
         expected = "C223091001"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_concatenated_serial_numbers(self):
         """
@@ -39,7 +40,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Номера подряд: c223013523С223092735."
         expected = "C223013523"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_mixed_case_and_spacing(self):
         """
@@ -47,7 +48,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Серийный номер: c223012961 с223100312. Следующий: C223090725."
         expected = "C223012961"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_multiple_concatenated_serial_numbers(self):
         """
@@ -55,7 +56,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Несколько номеров подряд: C223012430d252030021D251110041."
         expected = "C223012430"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_russian_and_latin_letters_mixed(self):
         """
@@ -63,7 +64,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Проверьте серийник: С111120102 и C223013523."
         expected = "C111120102"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_mixed_serial_numbers_with_repetitions(self):
         """
@@ -71,7 +72,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Серийный номер встречается дважды: CKМ01212505744 и CKM01212505744."
         expected = "CKM01212505744"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_mixed_russian_english_letters(self):
         """
@@ -79,7 +80,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Серийный номер встречается дважды: CKМ01212505744cKm01212505744."
         expected = "CKM01212505744"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_mixed_with_russian_letters_in_sequence(self):
         """
@@ -87,7 +88,7 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Ошибки с русскими буквами: С112040045, C222090950, C223090096."
         expected = "C112040045"
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
 
     def test_no_serial_numbers(self):
         """
@@ -95,7 +96,8 @@ class TestSerialNumberExtractor(unittest.TestCase):
         """
         text = "Текст без серийных номеров."
         expected = None
-        self.assertEqual(self.extractor.extract_serial_number_from_text(text), expected)
+        self.assertEqual(self.extractor.get_serial_number(text), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
